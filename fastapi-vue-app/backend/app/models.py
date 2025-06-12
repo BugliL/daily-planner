edd03e65ef3323e33bb3a48ff46237f93b4a58ca
BugliL <bugli.lorenzo@gmail.com>
2025-06-12 16:45:19 +0200

    feat: added models

--- fastapi-vue-app/backend/app/models.py
+++ fastapi-vue-app/backend/app/models.py
@@ -1,19 +1,33 @@
 from dataclasses import Field
 import enum
+import uuid
 from pydantic import BaseModel, Field
 from typing import Optional
-from datetime import datetime
+from datetime import date, datetime
 
 
 class Priority(enum.Enum):
     HIGH = "HIGH"
     IMPORTANT = "IMPORTANT"
     LOW = "LOW"
+
     
 
 class Task(BaseModel):
-    id: Optional[str] = None
+    id: Optional[str] = Field(default_factory=uuid.uuid4)
     title: Optional[str] = None
     completed: bool = False
     created_at: datetime = datetime.now()
     priority: Priority = Field(default=Priority.IMPORTANT)
+    
+
+class Note(BaseModel):
+    id: Optional[str] = None
+    content: str
+    created_at: datetime = datetime.now()
+    
+class Day(BaseModel):
+    day: date 
+    tasks: list[Task] = Field(default_factory=list)
+    note: Optional[Note] = None
+
