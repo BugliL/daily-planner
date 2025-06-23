58256c61d692263632384f7859bcaabbbddc6402
BugliL <bugli.lorenzo@gmail.com>
2025-06-23 16:01:58 +0200

    feat: working frontend

--- fastapi-vue-app/frontend/src/App.vue
+++ fastapi-vue-app/frontend/src/App.vue
@@ -1,29 +1,21 @@
- 
- <template>
-   <div id="app">
-     <h1>Daily Planner</h1>
-     <TaskList />
-   </div>
- </template>
- 
- <script>
- import TaskList from './components/TaskList.vue';
+<template>
+  <div id="app">
+    <h1>Daily Planner</h1>
+    <TaskList />
+  </div>
+</template>
 
- export default {
-   name: 'App',
-   components: {
-     TaskList
-   }
- }
- </script>
- 
- <style>
- #app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-align: center;
-   color: #2c3e50;
-   margin-top: 60px;
- }
- </style>
+<script setup>
+import TaskList from './components/TaskList.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fastapi-vue-app/frontend/src/components/TaskList.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+    <h1>Task list</h1>
+</template>
